0427297927f847ce166ca36a3f75ccffa4862002
sfyr111 <sfyr111@163.com>
2017-12-26 16:02:38 +0800

    fix

--- schema/methods.js
+++ schema/methods.js
@@ -8,12 +8,12 @@ var animalSchema = new Schema({ name: String, type: String });
 // 实例方法
 animalSchema.methods.findSimilarTypes = async function() {
   // this 为调用此方法的Model 实例对象
-  return this.model('Animal').find({ type: this.type })
+  return this.model('Animal').findOne({ type: this.type })
 };
 
 // 静态方法
 animalSchema.statics.findByName = async function(name) {
-  return this.find({ name })
+  return this.findOne({ name })
 }
 
 var Animal = mongoose.model('Animal', animalSchema);
@@ -24,5 +24,5 @@ Animal.create({ name: 'tom', type: 'cat' })
  */
 var dog = new Animal({ type: 'dog' });
 
-// dog.findSimilarTypes().then(data => console.log(data.name)) // woff
-Animal.findByName('tom').then(data => console.log(data))
+dog.findSimilarTypes().then(animal => console.log(animal.name)) // woff
+Animal.findByName('tom').then(animal => console.log(animal.name)) // tom
