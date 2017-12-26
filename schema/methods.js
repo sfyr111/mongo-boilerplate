94191b811b44ef228e777cfa41529bdce91e75df
sfyr111 <sfyr111@163.com>
2017-12-26 16:01:08 +0800

    静态方法

--- schema/methods.js
+++ schema/methods.js
@@ -5,9 +5,16 @@ const Schema = mongoose.Schema;
 
 var animalSchema = new Schema({ name: String, type: String });
 
+// 实例方法
 animalSchema.methods.findSimilarTypes = async function() {
+  // this 为调用此方法的Model 实例对象
   return this.model('Animal').find({ type: this.type })
 };
+
+// 静态方法
+animalSchema.statics.findByName = async function(name) {
+  return this.find({ name })
+}
 
 var Animal = mongoose.model('Animal', animalSchema);
 /* 
@@ -17,4 +24,5 @@ Animal.create({ name: 'tom', type: 'cat' })
  */
 var dog = new Animal({ type: 'dog' });
 
-dog.findSimilarTypes().then(dta => console.log(dta))
+// dog.findSimilarTypes().then(data => console.log(data.name)) // woff
+Animal.findByName('tom').then(data => console.log(data))
